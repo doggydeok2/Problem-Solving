66b9974a1bb4284fd3d160ce266010e0b8ae4502
이홍덕 <doggydeok2@naver.com>
2022-07-28 15:23:59 +0900

    22.07.28.

---
+++ BOJ/1238 파티.py
@@ -0,0 +1,35 @@
+import heapq
+
+
+def dijkstra(_adl):
+    dij = [float('inf')] * (N + 1)
+    prq = [(0, X)]
+    dij[X] = 0
+    while prq:
+        t, e = heapq.heappop(prq)
+        if dij[e] < t:
+            continue
+        for nxt in _adl[e]:
+            nt, ne = nxt
+            if t + nt < dij[ne]:
+                dij[ne] = t + nt
+                heapq.heappush(prq, (t + nt, ne))
+    return dij
+
+
+N, M, X = map(int, input().split())
+
+dij = [[float('inf') for _ in range(N + 1)] for __ in range(N + 1)]
+for i in range(1, N + 1):
+    dij[i][i] = 0
+
+go_adl = [[] for _ in range(N + 1)]
+from_adl = [[] for _ in range(N + 1)]
+for _ in range(M):
+    s, e, t = map(int, input().split())
+    go_adl[s].append((t, e))
+    from_adl[e].append((t, s))
+
+gotoX = dijkstra(go_adl)
+fromtoX = dijkstra(from_adl)
+print(max([gotoX[i] + fromtoX[i] for i in range(1, N + 1)]))
